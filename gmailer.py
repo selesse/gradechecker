a462b9885ebc76595c6ac884b99e4d9fedc7d9a9
Alex Selesse <alex@selesse.com>
2012-09-11 16:50:17 -0400

    Added MIMEType messages to have "from" and easier (potential) HTML support.

--- gmailer.py
+++ gmailer.py
@@ -1,10 +1,10 @@
-class gmailer:
+class gmailer(object):
   def __init__(self, smtp_login, smtp_password, email_from):
     import smtplib
 
     # if no email-from is supplied, use the name before the @
     #   i.e. "email@example.com" becomes "email"
-    if len(email_from) == 0:
+    if not email_from:
       email_from = smtp_login[0:smtp_login.find("@")]
 
     smtp_obj = smtplib.SMTP('smtp.gmail.com', 587)
@@ -14,19 +14,22 @@ class gmailer:
     smtp_obj.login(smtp_login, smtp_password)
 
     self.smtp_obj = smtp_obj
-    self.email_from = email_from
     self.smtp_login = smtp_login
+    self.email_from = email_from
     self.smtp_password = smtp_password
 
   def send_mail(self, recipients, subject, message):
+    from email.MIMEMultipart import MIMEMultipart
+    from email.MIMEText import MIMEText
+
     # if recipients is not a list (i.e. single email), make it so
     if not isinstance(recipients, list):
       recipients = [recipients]
 
-    body = 'To: %s\n' % ",".join(recipients)
-    body += 'From: %s\n' % self.smtp_login
-    body += 'Subject: %s\n' % subject
-    body += '\n'
-    body += message
+    msg = MIMEMultipart()
+    msg['From'] = '"%s" %s' % (self.email_from, self.smtp_login)
+    msg['To'] = ",".join(recipients)
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message, 'plain'))
 
-    self.smtp_obj.sendmail(self.email_from, recipients, body)
+    self.smtp_obj.sendmail(self.email_from, recipients, msg.as_string())
